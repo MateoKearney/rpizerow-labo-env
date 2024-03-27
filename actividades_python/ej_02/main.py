a7b45ac987b6ad94dab5e4a9a7dbfdf42a3b4b8a
“JuanIgnacioTorresH” <juanignaciotorr@gmail.com>
2024-03-27 19:35:48 +0000

    feat: Realicé el ejercicio 2

--- actividades_python/ej_02/main.py
+++ actividades_python/ej_02/main.py
@@ -9,14 +9,15 @@ ledverde = LED(13)
 
 #genero un bucle que prenda y suspenda los LEDS cada x tiempo
 while True:
+#declaro que el led rojo que se prenda un segundo
 	ledrojo.on()
 	sleep(1)
 	ledrojo.off()
-
+#declaro el funcionamiento del led verde durante medio segundo
 	ledverde.on()
 	sleep(0.5)
 	ledverde.off()
-
+#declaro el funcionamiento del led azul por 4 segundos
 	ledazul.on()
 	sleep(4)
 	ledazul.off()
