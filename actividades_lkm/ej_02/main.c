42bc9ef3c67ad600e9101b005a16b842983b9a3c
MateoKearney <mateokearney80mail.com>
2024-05-02 21:06:37 +0100

    feat: Realicé el ejercicio 2 de LKM

--- actividades_lkm/ej_02/main.c
+++ actividades_lkm/ej_02/main.c
@@ -12,7 +12,28 @@ static struct task_struct *kthread_2 = NULL;
 
 // TODO:
 // Funciones para los hilos
+static int hilo_1(void *params) {
+	// Creacion de variables
 
+	// Bucle mientras que la tarea no sea detenida
+	while(!kthread_should_stop()) {
+		printk("Kearney_Mateo_ej02: Hola desde el Hilo N°1");
+		msleep(1000);
+	}
+	// Finalizacion del hilo
+	return 0;
+}
+static int hilo_2(void *params) {
+	// Creacion de variables
+
+	// Bucle mientras que la tarea no sea detenida
+	while(!kthread_should_stop()) {
+		printk("Kearney_Mateo_ej02: Hola desde el Hilo N°2");
+		msleep(1000);
+	}
+	// Finalizacion del hilo
+	return 0;
+}
 /**
  * @brief Se llama cuando se carga en el kernel
 */
@@ -21,6 +42,20 @@ static int __init ej02_module_init(void) {
 	// - Crear ambos hilos
 	// - Verificar que se hayan podido crear
 	// - Correr hilos
+	kthread_1 = kthread_create(hilo_1, NULL, "kthread_1");
+    if(kthread_1 == NULL) {
+        printk(KERN_INFO "Kearney_Mateo_ej02: No se pudo hacer el Primer Hilo");
+		return -1;
+    }
+
+    kthread_2 = kthread_create(hilo_2, NULL, "kthread_2");
+    if(kthread_2 == NULL) {
+        printk(KERN_INFO "Kearney_Mateo_ej02: No se pudo hacer el Segundo Hilo");
+		return -1;
+    }
+	wake_up_process(kthread_1);
+	wake_up_process(kthread_2);
+
 	return 0;
 }
 
@@ -29,6 +64,8 @@ static int __init ej02_module_init(void) {
 */
 static void __exit ej02_module_exit(void) {
 	// TODO:
+	kthread_stop(kthread_1);
+	kthread_stop(kthread_2);
 	// - Detener hilos
 }
 
